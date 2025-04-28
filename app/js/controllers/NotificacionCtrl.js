ca338c81f3f92df01df10fc5962fa2e77881e1bf
victor steven <visteblanco@gmail.com>
2025-04-27 21:51:40 -0500

    ajuste timer NotificacionCtrl

--- app/js/controllers/NotificacionCtrl.js
+++ app/js/controllers/NotificacionCtrl.js
@@ -15,7 +15,7 @@ angular.module('Frosch')
                 $state.go('jugar.chico.principal.termino');
             else
                 $state.go('jugar.chico.principal')
-        }, 700);
+        }, 850);
 
         $scope.$on("$destroy",
             function (event) {
